7022b9165bd5f43e538bd7baf5dcd4cd947d22c8
JPLeon18 <juanpabloleonmera@gmail.com>
2018-05-14 15:01:26 -0500

    Rutas agregadas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
+
+const app_routes: Routes =  [
+    { path: '', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'portafolio-item', component: PortafolioItemComponent },
+    { path: '**', component: PortafolioComponent }
+
+
+];
+
+export const app_routing = RouterModule.forRoot(app_routes, {useHash: true});
