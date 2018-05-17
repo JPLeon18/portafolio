ef281e967b656e70e4c57ebbaeb09ce1e262656d
JPLeon18 <juanpabloleonmera@gmail.com>
2018-05-16 23:36:47 -0500

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,28 @@ export class InformacionService {
 
     info: any = {};
     cargada: boolean = false;
+    cargada_sobre_nosotro: boolean = false;
+    equipo:any[] = [];
 
   constructor(public http: Http ) {
-     this.http.get("assets/data/info.pagina.json")
-                  .subscribe( data =>{
-                      console.log(data.json());
-                      this.cargada = true;
-                      this.info = data.json();
-                  } );
+    this.carga_info();
+        this.carga_sobre_nosotros();
   }
 
+  public carga_info(){
+      this.http.get("assets/data/info.pagina.json")
+          .subscribe( data =>{
+              this.cargada = true;
+              this.info = data.json();
+          } );
+  }
+
+  public carga_sobre_nosotros(){
+      this.http.get("https://paginaweb-e2549.firebaseio.com/equipo.json")
+          .subscribe( data =>{
+              console.log(data.json());
+              this.cargada_sobre_nosotro = true;
+              this.equipo = data.json();
+          } );
+  }
 }
