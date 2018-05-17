25082259aa739928375190f3a9bd1052f9af0baf
JPLeon18 <juanpabloleonmera@gmail.com>
2018-05-16 22:25:30 -0500

    servicio creado, header y footer

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,10 +4,15 @@ import { Http } from '@angular/http';
 @Injectable()
 export class InformacionService {
 
+    info: any = {};
+    cargada: boolean = false;
+
   constructor(public http: Http ) {
      this.http.get("assets/data/info.pagina.json")
                   .subscribe( data =>{
-                      console.log(data);
+                      console.log(data.json());
+                      this.cargada = true;
+                      this.info = data.json();
                   } );
   }
 
