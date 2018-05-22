b6b285ad69ec237268677295851e3cd759d08470
JPLeon18 <juanpabloleonmera@gmail.com>
2018-05-21 19:41:46 -0500

    pagina de busqueda empezada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
+import { SearchComponent } from './components/search/search.component';
 
 const app_routes: Routes =  [
     { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'portafolio-item/:id', component: PortafolioItemComponent },
+    { path: 'buscar/:termino', component: SearchComponent },
     { path: '**', component: PortafolioComponent }
 
 
